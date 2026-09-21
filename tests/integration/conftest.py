ade4ab4fc186e2e58f36d557175ad9a26f91774d
Iman Enami <iman.enami@canonical.com>
2026-09-21 15:58:52 +0400

    feat: add ARM support

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,9 +2,14 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import glob
+import os
+import platform
 
 import pytest
 from pytest_operator.plugin import OpsTest
+
+TEST_BASE = "ubuntu@24.04"
 
 
 def pytest_addoption(parser):
@@ -25,6 +30,16 @@ def kafka_version(request: pytest.FixtureRequest) -> int:
 @pytest.fixture(scope="module")
 async def kafka_app_charm(ops_test: OpsTest):
     """Build the application charm."""
-    charm_path = "."
-    charm = await ops_test.build_charm(charm_path)
-    return charm
+    if "CI" not in os.environ:
+        charm_path = "."
+        charm = await ops_test.build_charm(charm_path)
+        return charm
+
+    base = TEST_BASE
+    processor = platform.processor()
+    architecture = "arm64" if processor == "aarch64" else "amd64"
+    match = glob.glob(f"*{base}-{architecture}.charm")
+    if not match:
+        raise RuntimeError(f"Can not find appropriate charm file for {base=} {architecture=}")
+
+    return match[0]
